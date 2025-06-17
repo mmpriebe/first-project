9386ae5680426a6ae319896c7992ebc4a317e4b3
mmpriebe <marciano.priebe@gmail.com>
2025-06-17 18:42:02 -0300

    Adicionado arquivo css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react"
+import './assets/app.css'
 
 export default function App() {
 
   return (
     <>
-      <h1 style={{color: '#555', fontSize: '40px'}}>Titulo do Blog</h1>
-      <h2 style={{color: '#555', fontSize: '30px'}}>Subtitulo do blog</h2>
+      <h1 className="titulo">Titulo do Blog</h1>
+      <h2>Subtitulo do blog</h2>
 
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
         Ratione architecto excepturi officia ab officii.</p>
